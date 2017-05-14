b292006625163118578d49ac5915cc7674b11db3
harshul1610 <harshulrobo@gmail.com>
2017-05-14 17:41:07 +0530

    renaming

---
+++ TransferLearningScripts/tl_feature_extract/train_cnf.py
@@ -0,0 +1,33 @@
+from __future__ import division, print_function, absolute_import
+
+import tensorflow as tf
+
+from tefla.core.lr_policy import StepDecayPolicy
+from tefla.utils import util
+
+cnf = {
+    'name': __name__.split('.')[-1],
+    'classification': True,
+    'iterator_type': 'queued',  # parallel or queued
+    'batch_size_train': 16,
+    'batch_size_test': 16,
+    'l2_reg': 0.005,
+    'aug_params': {
+        'zoom_range': (1 / 1.1, 1.1),
+        'rotation_range': (-5, 5),
+        'shear_range': (1, 1),
+        'translation_range': (-20, 20),
+        'do_flip': True,
+        'allow_stretch': True,
+    },
+    'num_epochs': 15,
+    'summary_every': 5,
+    'lr_policy': StepDecayPolicy(
+        schedule={
+            0: 0.001,
+            10:0.0001,
+        }
+    ),
+    'optimizer': tf.train.AdamOptimizer(),
+    'validation_scores': [('validation accuracy', util.accuracy_wrapper), ('validation kappa', util.kappa_wrapper)],
+}
