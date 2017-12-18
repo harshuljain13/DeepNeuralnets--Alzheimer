0e65cabc5bbc641f378c70a61976b51ded35641d
harshul <harshulrobo@gmail.com>
2017-12-18 15:13:10 +0530

    Cleansing

--- webapp/videoclassification/test_nii.py
+++ webapp/videoclassification/test_nii.py
@@ -11,7 +11,7 @@ import keras
 home_files_dir = '/home/ubuntu/Select_original_fmri/'
 #output_dir='/home/ubuntu/final_src/DeepNeuralnets--Alzheimer/videoclassification/'
 
-model = Sequential()
+loaded_model = Sequential()
 # load json and create model
 json_file = open('arch.json', 'r')
 loaded_model_json = json_file.read()
@@ -32,22 +32,23 @@ for class_ in classes:
         
         count = 0
         for file_ in files:
-            print 'working on ' + file_ + '...'
-            try:
+            #print 'working on ' + file_ + '...'
+            #try:
                 # extracting data from nii files
                 x = nib.load(os.path.join(home_files_dir+"niifiles/" , class_) + '/' + file_).get_data()
-                #print("x_shape",x.shape)
+                print("x_shape",x.shape)
                 x_arr=np.array(x)
                 #print ("x_arr shape : ",x_arr.shape)
                 #converting X_arr into 3_D array.........
                 x_3d = x_arr.reshape(x_arr.shape[0],x_arr.shape[1],x_arr.shape[2]*x_arr.shape[3])
-                out=model.predict_classes(x_3d)
+                out=loaded_model.predict_classes(x_3d)
                 print out
                 #np.save('output_array' +'/'+ class_ + '/'+str(file_)+str(count),x_3d)
                 count+=1
                 #print("3d array shape : ",x_3d.shape)
-
-            except:
+                break
+            #except:
                 print("in last except block")
                 print file_
+                break
 
