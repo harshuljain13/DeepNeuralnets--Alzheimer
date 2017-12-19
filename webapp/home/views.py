f59777c177c8c1062b2bb6cfbd984cbbc3325e48
harshul1610 <harshulrobo@gmail.com>
2017-12-20 00:08:01 +0530

    webapp fix

--- webapp/home/views.py
+++ webapp/home/views.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render, HttpResponse
-
-# Create your views here.
+import videoclassification as vc
 
 
 def index_view(request):
     if request.method == 'POST':
-        return HttpResponse('uploaded')
+        filename = request.FILES['niifile'].name
+
+        return render(request, 'home/index.html', {'filename': filename})
     if request.method == 'GET':
         return render(request, 'home/index.html')
