8d7bc8d500d6e867bc4e5d20fabdb6d5810d0b20
harshul1610 <harshulrobo@gmail.com>
2017-12-24 09:41:30 +0530

    display dummy result after task completion

--- webapp/home/tasks.py
+++ webapp/home/tasks.py
@@ -12,10 +12,29 @@ import os
 @task(name="predict_nii_file")
 def predict_nii_file(filename):
     #print current_task
-    current_task.update_state(state='PROGRESS', meta={'process_percent':30})
+    # loading the model
+    current_task.update_state(state='PROGRESS', meta={'process_percent':0})
+    loaded_model = Sequential()
+    current_task.update_state(state='PROGRESS', meta={'process_percent':2})
+    json_file = open('videoclassification/arch.json')
+    current_task.update_state(state='PROGRESS', meta={'process_percent':5})
+    loaded_model_json = json_file.read()
+    current_task.update_state(state='PROGRESS', meta={'process_percent':10})
+    json_file.close()
+    loaded_model = keras.models.model_from_json(loaded_model_json)
+    current_task.update_state(state='PROGRESS', meta={'process_percent':20})
+    #loaded_model.load_weights("videoclassification/weigths.h5")
+    #current_task.update_state(state='PROGRESS', meta={'process_percent':50})
+
+    # reading the nii file
     x = nib.load(os.path.join('media', filename)).get_data()
-    #time.sleep(30)
-    return 'DONE'
+    x_arr = np.array(x)
+    a_3d = x_arr.reshape(x_arr.shape[0], x_arr.shape[1], x_arr.shape[2]*x_arr.shape[3])
+    current_task.update_state(state='PROGRESS', meta={'process_percent':70})
+
+    # predict the classes for the nii file
+    #out = loaded_model.predict_classes(x_3d)
+    return 'Alzheimer detected - 72%, MCI detected - 10%, Normal - 18%'
 
 @shared_task
 def fft_random(n):
@@ -37,7 +56,12 @@ def get_task_status(task_id):
     status = task.status
     process_percent = 5
     if status == 'SUCCESS':
+        result = task.result
+        #print result
+        #print type(result)
+        result_data = result.split(',')
         process_percent = 100
+        return {'status':status, 'process_percent':process_percent, 'result_data':result_data}
     if status == 'PROGRESS':
         process_percent = task.info['process_percent']
-    return {'status':status, 'process_percent':process_percent}
+        return {'status':status, 'process_percent':process_percent}
